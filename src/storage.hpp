35517dfeb9536eb7d949456d7e046e905de5f576
yewentao <zhyanwentao@126.com>
2023-08-19 15:43:56 +0800

    support cuda index const get

--- src/storage.hpp
+++ src/storage.hpp
@@ -22,8 +22,8 @@ class Storage {
   ~Storage() = default;
   Storage& operator=(const Storage& other) = delete;
 
-  data_t operator[](size_t idx) const { return data_ptr_.get()[idx]; }
-  data_t& operator[](size_t idx) { return data_ptr_.get()[idx]; }
+  data_t operator[](size_t idx) const;
+  data_t& operator[](size_t idx);
   data_t* data() { return data_ptr_.get(); }
   const Device& device() const { return device_; }
   size_t nbytes() const { return nbytes_; }
@@ -31,6 +31,8 @@ class Storage {
  private:
   size_t nbytes_;
   Device device_;
+  // Note: it seems that shared_ptr is used for controlling cuda memory here
+  // But it actually has a deleter for cuda, see allocator.hpp for details.
   std::shared_ptr<data_t> data_ptr_;
 };
 
